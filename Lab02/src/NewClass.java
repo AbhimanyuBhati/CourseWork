d3fce7a968575cd9a5d726c9307e90ac7477aa10
Abhimanyu Bhati <62279759+AbhimanyuBhati@users.noreply.github.com>
2022-01-27 16:45:07 +0800

    Add files via upload

--- Lab02/src/NewClass.java
+++ Lab02/src/NewClass.java
@@ -12,23 +12,33 @@ import java.util.Scanner;
 
 public class NewClass {
 
-    
     public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);    
-    NewClass obj = new NewClass();
+
+        //Calling object of scanner class to read inputs
+        Scanner reader = new Scanner(System.in);
+
+        //Calling a constructor of the class to get access to other functions
+        NewClass obj = new NewClass();
+
+        //Reading Input from User
         System.out.println("Enter number of throws");
         int numThrows = reader.nextInt();
-        double PI=((obj.Ans(numThrows)/numThrows)*4);
-System.out.println(PI);
 
+        //Approximating Value of PI 
+        double PI = ((obj.Ans(numThrows) / numThrows) * 4);
+        System.out.println(PI);
     }
 
+    //'Method()' to calculate the number of points in the circle
     double Ans(int numThrows) {
         int hits = 0;
         for (int i = 0; i < numThrows; i++) {
+
+            //Assigning Random points on the first quarter of the square-circle diagram
             double xpos = Math.random();
             double ypos = Math.random();
 
+            //If the point is within the circle, we increment 'hits'
             if (Math.sqrt((xpos * xpos) + (ypos * ypos)) < 1) {
                 System.out.println(hits);
                 hits++;
